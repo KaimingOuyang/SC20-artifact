96559ea8c6e2a463fe9c607818b97253a4d84b9a
Kaiming Ouyang <kouya001@ucr.edu>
2020-05-01 18:36:44 -0500

    fix fig1 submit-job script bug

--- results/fig1/submit-job.sh
+++ results/fig1/submit-job.sh
@@ -4,9 +4,10 @@ ROOT_DIR=`pwd`/../..
 export PATH=${ROOT_DIR}/installed/mpich-std/bin:$PATH
 
 cd ${ROOT_DIR}/app/miniGhost && \
-git checkout remotes/origin/communication-imbalance-profile && \
-make clean && \
-cd ref && make
+git checkout communication-imbalance-profile && \
+cd ref && \
+make clean && make && \
+mv miniGhost.x miniGhost.x-profile
 
 stat=$?
 if [ $stat -ne 0 ]; then
@@ -16,7 +17,7 @@ fi
 
 # slurm script submission
 cd ${ROOT_DIR}/results/fig1 && \
-sbatch ${SLURM_PARAM} minighost-comm.job ${ROOT_DIR}/app/miniGhost/ref/miniGhost.x
+sbatch ${SLURM_PARAM} minighost-comm.job ${ROOT_DIR}/app/miniGhost/ref/miniGhost.x-profile
 
 
 
