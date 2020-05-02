07e254c476b1f8219f946065b5e3f4e76d99eab3
Kaiming Ouyang <kouya001@ucr.edu>
2020-05-02 10:03:19 -0500

    add cd .. after enter each fig dir

--- draw-figs.sh
+++ draw-figs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
+export ROOT_DIR=$(pwd)
 folders=(fig1 fig3 fig4 fig5 fig6 fig7 fig8 fig9 fig10 fig11 fig12)
 for folder in ${folders[@]}; do
     cd results/${folder} && \
-    sh draw-fig.sh
+    sh draw-fig.sh && cd ${ROOT_DIR}
     
     stats=$?
     if [ $stats != 0 ]; then
--- submit-jobs.sh
+++ submit-jobs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export ROOT_DIR=$(pwd)
 export BDW_PARTITION="-p bdwall"
 export KNL_PARTITION="-p knlall"
 export BDW_SLURM_PARAM="${BDW_PARTITION}"
@@ -8,7 +9,7 @@ export KNL_SLURM_PARAM="${KNL_PARTITION}"
 folders=(fig1 fig3 fig4 fig5 fig6 fig7 fig8 fig9 fig10 fig11 fig12)
 for folder in ${folders[@]}; do
     cd results/${folder} && \
-    sh submit-job.sh
+    sh submit-job.sh && cd ${ROOT_DIR}
     
     stats=$?
     if [ $stats != 0 ]; then
