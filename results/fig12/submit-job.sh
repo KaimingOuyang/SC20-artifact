38ffb2ce4dcd30a021f8292ef9e69f12d440d1a7
Kaiming Ouyang <kouya001@ucr.edu>
2020-05-02 12:05:18 -0500

    fix BSPMM submission path

--- results/fig12/submit-job.sh
+++ results/fig12/submit-job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT_DIR=$(pwd)/../../
+ROOT_DIR=$(pwd)/../..
 CURR_DIR=$(pwd)
 ############################
 # Broadwell bspmm Test
@@ -17,10 +17,17 @@ if [ $stats != 0 ]; then
 	exit 1
 fi
 
+
 nodes=(1 2 3 4 6 8 10 12 14 16 24 32)
 #jobid=
+cd ${CURR_DIR} && \
 for N in ${nodes[@]}; do
 	sbatch -N ${N} -t 6:00:00 bdw-strong-scaling.job ${ROOT_DIR}/app/BSPMM/bspmm_profile_original bspmm_profile_original.out ${N}
+	stats=$?
+	if [ $stats != 0 ]; then
+		echo "BSPMM original submission error"
+		exit 1
+	fi
 done
 
 export PATH=${ROOT_DIR}/installed/mpich-throughput-aware-bdw/bin:$PATH
@@ -29,6 +36,12 @@ make clean && \
 make LDFLAGS="-L${ROOT_DIR}/lib/openblas/lib -Wl,-rpath=${ROOT_DIR}/lib/openblas/lib -lopenblas" && \
 mv bspmm_profile bspmm_profile_throughput
 
+cd ${CURR_DIR} && \
 for N in ${nodes[@]}; do
 	sbatch -N ${N} -t 6:00:00 bdw-strong-scaling.job ${ROOT_DIR}/app/BSPMM/bspmm_profile_throughput bspmm_profile_throughput.out ${N}
+	stats=$?
+	if [ $stats != 0 ]; then
+		echo "BSPMM throughput submission error"
+		exit 1
+	fi
 done
