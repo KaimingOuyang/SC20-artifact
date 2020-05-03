95ce75381ae46269ad73b9a5c65206aa36645526
Kaiming Ouyang <kouya001@ucr.edu>
2020-05-03 16:57:51 -0500

    fix ylim in fig7

--- results/fig7/draw-fig.sh
+++ results/fig7/draw-fig.sh
@@ -4,7 +4,8 @@ export TOOL_DIR=`pwd`/../tools
 
 function parse_speedup {
     local file=$1
-    echo $(cat ${file} | awk 'BEGIN{
+    local type=$2
+    echo $(cat ${file} | awk -v type=${type} 'BEGIN{
         cnt = 0;
     }{
         key[cnt] = $1;
@@ -14,11 +15,17 @@ function parse_speedup {
     }
     END{
         for(i=0;i<cnt;++i){
-            
-            if(i != cnt - 1)
-                printf("%d\\t", key[i]);
-            else
-                printf("%d", key[i]);
+            if (type == "inter"){
+                if(i != cnt - 1)
+                    printf("%d\\t", key[i] / 2);
+                else
+                    printf("%d", key[i] / 2);
+            }else{
+                if(i != cnt - 1)
+                    printf("%d\\t", key[i]);
+                else
+                    printf("%d", key[i]);
+            }
         }
         printf(" ");
         for(i=0;i<cnt;++i){
@@ -41,11 +48,11 @@ function generate_datafile {
     local type=$1
     local fig=$2
     cp data.template ${fig}
-    line_num=17
+    line_num=15
     tile_dims=(128 256 512 1024)
     for tile_dim in ${tile_dims[@]}; do
         python3 ${TOOL_DIR}/parse_speedup.py accumulate-original-${type}-bdw.out.${tile_dim} accumulate-throughput-${type}-bdw.out.${tile_dim} > accumulate-throughput-${type}-bdw.speedup.${tile_dim}
-        strings=$(parse_speedup accumulate-throughput-${type}-bdw.speedup.${tile_dim})
+        strings=$(parse_speedup accumulate-throughput-${type}-bdw.speedup.${tile_dim} ${type})
         IFS=' ' read -r -a results <<< ${strings}
         key=${results[0]}
         values=${results[1]}
@@ -71,20 +78,21 @@ if [ $stats != 0 ]; then
 fi
 
 # draw fig7 (b)
-key=$(generate_datafile inter fig7b.data) && \
-tmp=$(echo ${key} | tr "\t" " ") && \
-IFS=' ' read -r -a results <<< ${tmp} && \
-for i in ${!results[@]}; do
-    if [ $i == 0 ]; then
-        key="$((${results[i]} / 2))"
-    else
-        key="${key}\t$((${results[i]} / 2))"
-    fi
-done
-sed -i "4c ${key}" ${fig} && \
-sed -i "4 s/^/\t/" ${fig} && \
-sed -i "6c #Processes (On Node 1)" ${fig} && \
-sed -i "6 s/^/\t/" ${fig} && \
+key=$(generate_datafile inter fig7b.data)
+# tmp=$(echo ${key} | tr "\t" " ") && \
+# echo ${tmp} && \
+# IFS=' ' read -r -a results <<< ${tmp} && \
+# for i in ${!results[@]}; do
+#     if [ $i == 0 ]; then
+#         key="$((${results[i]} / 2))"
+#     else
+#         key="${key}\t$((${results[i]} / 2))"
+#     fi
+# done
+sed -i "4c ${key}" fig7b.data && \
+sed -i "4 s/^/\t/" fig7b.data && \
+sed -i "6c #Processes (On Node 1)" fig7b.data && \
+sed -i "6 s/^/\t/" fig7b.data && \
 python3 ${TOOL_DIR}/Painter.py fig7b.data
 
 stats=$?
