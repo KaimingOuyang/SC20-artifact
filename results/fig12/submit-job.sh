670bf1ebbd6aa3d0cf05e87f8b6f8740d89b9c65
Kaiming Ouyang <kouya001@ucr.edu>
2020-05-03 15:45:31 -0500

    set up job dependency in fig12

--- results/fig12/submit-job.sh
+++ results/fig12/submit-job.sh
@@ -22,7 +22,12 @@ nodes=(1 2 3 4 6 8 10 12 14 16 24 32)
 #jobid=
 cd ${CURR_DIR} && \
 for N in ${nodes[@]}; do
-	sbatch -N ${N} -t 12:00:00 bdw-strong-scaling.job ${ROOT_DIR}/app/BSPMM/bspmm_profile_original bspmm_profile_original.out ${N}
+	if [ $N == 1 ]; then
+		dep_id=$(sbatch -N ${N} -t 12:00:00 bdw-strong-scaling.job ${ROOT_DIR}/app/BSPMM/bspmm_profile_original bspmm_profile_original.out ${N} | awk '{print $4}')
+	else
+		dep_id=$(sbatch --dependency=afterok:${dep_id} -N ${N} -t 12:00:00 bdw-strong-scaling.job ${ROOT_DIR}/app/BSPMM/bspmm_profile_original bspmm_profile_original.out ${N} | awk '{print $4}')
+	fi
+
 	stats=$?
 	if [ $stats != 0 ]; then
 		echo "BSPMM original $N nodes submission error"
@@ -38,7 +43,11 @@ mv bspmm_profile bspmm_profile_throughput
 
 cd ${CURR_DIR} && \
 for N in ${nodes[@]}; do
-	sbatch -N ${N} -t 12:00:00 bdw-strong-scaling.job ${ROOT_DIR}/app/BSPMM/bspmm_profile_throughput bspmm_profile_throughput.out ${N}
+	if [ $N == 1 ]; then
+		dep_id=$(sbatch -N ${N} -t 12:00:00 bdw-strong-scaling.job ${ROOT_DIR}/app/BSPMM/bspmm_profile_throughput bspmm_profile_throughput.out ${N} | awk '{print $4}')
+	else
+		dep_id=$(sbatch --dependency=afterok:${dep_id} -N ${N} -t 12:00:00 bdw-strong-scaling.job ${ROOT_DIR}/app/BSPMM/bspmm_profile_throughput bspmm_profile_throughput.out ${N} | awk '{print $4}')
+	fi
 	stats=$?
 	if [ $stats != 0 ]; then
 		echo "BSPMM throughput $N nodes submission error"
